b9e7bde406ad6051febb0f9c02fb5428e857a19a
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-18 00:31:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,7 @@ end
 
 def selective_tweet_shortener(tweets)
   #return tweets.size
-  tweets.each do |tweet|
+  tweets.map { |e|  } do |tweet|
     if tweet.length >= 140
       tweet[0...130]
       i = 0
