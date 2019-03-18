ecaa2b9b7297eab968ea64c61772f4bc06d3fca4
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-18 03:44:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -49,7 +49,7 @@ end
 
 def shortened_tweet_truncator(tweets)
 
-  if word_substituter(tweet).length > 140
+  if word_substituter(tweets).length > 140
     word_substituter(tweets[0...137])+ "..."
   else
    tweets
